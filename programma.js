7e05ed54148dd9972573f5a780851d5b1ddb7f83
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2020-10-25 11:43:11 +0100

    lezione del 20-10-2020

--- programma.js
+++ programma.js
@@ -1,7 +1,32 @@
-let posizione = 0;
-let direzione = +1;
 let velocita;
 let diametro;
+let palla0;
+let palla1;
+
+function Palla() {
+    this.posizione = random(width);
+    this.direzione = +1;
+    this.red = random(255);
+    this.green = random(255);
+    this.blu = random(255);
+
+    this.cambia = function () {
+        this.posizione = this.posizione + this.direzione * velocita.value();
+
+        if (this.posizione + diametro.value() / 2 > width) {
+            this.direzione = -1;
+        }
+
+        if (this.posizione - diametro.value() / 2 < 0) {
+            this.direzione = +1;
+        }
+    }
+
+    this.disegna = function () {
+        fill(this.red, this.green, this.blu, 200);
+        ellipse(this.posizione, height / 2, diametro.value());
+    }
+}
 
 function setup() {
     createCanvas(800, 800);
@@ -9,22 +34,17 @@ function setup() {
     diametro = createSlider(1, width, width / 5);
 
     console.log(width, height);
+
+    palla0 = new Palla();
+    palla1 = new Palla();
 }
 
 function draw() {
-    posizione = posizione + direzione * velocita.value();
-
     background(0, 0, 0);
+    
+    palla0.cambia();
+    palla0.disegna();
 
-    fill(74, 74, 26);
-
-    ellipse(posizione, height / 2, diametro.value());
-
-    if (posizione + diametro.value() / 2 > width) {
-        direzione = -1;
-    }
-
-    if (posizione - diametro.value() / 2 < 0) {
-        direzione = +1;
-    }
+    palla1.cambia();
+    palla1.disegna();
 }
