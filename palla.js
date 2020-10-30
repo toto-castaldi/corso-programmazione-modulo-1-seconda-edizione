323620edc459a6c54bb2220e9c5d075d80dba287
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2020-10-30 07:44:43 +0100

    lezione del 27/10/2020

---
+++ palla.js
@@ -0,0 +1,38 @@
+function Palla(pX, pY) {
+    
+    this.posizioneX = pX;
+    this.direzioneX = +1;
+
+    this.posizioneY = pY;
+    this.direzioneY = +1;
+
+    this.red = random(255);
+    this.green = random(255);
+    this.blu = random(255);
+
+    this.cambia = function () {
+        this.posizioneX = this.posizioneX + this.direzioneX * velocita.value();
+        this.posizioneY = this.posizioneY + this.direzioneY * velocita.value();
+
+        if (this.posizioneX + diametro.value() / 2 > width) {
+            this.direzioneX = -1;
+        }
+
+        if (this.posizioneX - diametro.value() / 2 < 0) {
+            this.direzioneX = +1;
+        }
+
+        if (this.posizioneY + diametro.value() / 2 > height) {
+            this.direzioneY = -1;
+        }
+
+        if (this.posizioneY - diametro.value() / 2 < 0) {
+            this.direzioneY = +1;
+        }
+    }
+
+    this.disegna = function () {
+        fill(this.red, this.green, this.blu, 200);
+        ellipse(this.posizioneX, this.posizioneY, diametro.value());
+    }
+}
--- programma.js
+++ programma.js
@@ -3,40 +3,21 @@ let diametro;
 let palla0;
 let palla1;
 
-function Palla() {
-    this.posizione = random(width);
-    this.direzione = +1;
-    this.red = random(255);
-    this.green = random(255);
-    this.blu = random(255);
-
-    this.cambia = function () {
-        this.posizione = this.posizione + this.direzione * velocita.value();
-
-        if (this.posizione + diametro.value() / 2 > width) {
-            this.direzione = -1;
-        }
-
-        if (this.posizione - diametro.value() / 2 < 0) {
-            this.direzione = +1;
-        }
-    }
-
-    this.disegna = function () {
-        fill(this.red, this.green, this.blu, 200);
-        ellipse(this.posizione, height / 2, diametro.value());
-    }
-}
-
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(800, 600);
     velocita = createSlider(0, 20, 5);
     diametro = createSlider(1, width, width / 5);
 
-    console.log(width, height);
+    palla0 = new Palla(100,30);
+    palla1 = new Palla(300,150);
+}
 
-    palla0 = new Palla();
-    palla1 = new Palla();
+function mouseClicked() {
+    if (mouseX > width /2) {
+        palla0 = new Palla(mouseX, mouseY);
+    } else {
+        palla1 = new Palla(mouseX, mouseY);
+    }
 }
 
 function draw() {
